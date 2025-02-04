55e9bd28955e0d6f3ff8cf20106f8b4e2a2976bf
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-02-03 16:14:22 -0800

    Add /authorize redirect and /token proxy

--- src/descope.ts
+++ src/descope.ts
@@ -2,7 +2,7 @@ import DescopeClient from '@descope/node-sdk';
 
 const DESCOPE_PROJECT_ID = process.env.DESCOPE_PROJECT_ID;
 const DESCOPE_MANAGEMENT_KEY = process.env.DESCOPE_MANAGEMENT_KEY;
-
+const DESCOPE_BASE_URL = process.env.DESCOPE_BASE_URL;
 if (!DESCOPE_PROJECT_ID || !DESCOPE_MANAGEMENT_KEY) {
     throw new Error('DESCOPE_PROJECT_ID and DESCOPE_MANAGEMENT_KEY must be set');
 }
@@ -12,4 +12,4 @@ const descope = DescopeClient({
     managementKey: DESCOPE_MANAGEMENT_KEY,
 });
 
-export { descope, DESCOPE_PROJECT_ID };
+export { descope, DESCOPE_PROJECT_ID, DESCOPE_BASE_URL };
