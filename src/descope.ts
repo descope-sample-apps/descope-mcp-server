d8d986494e933e7ade125e1083017a8ce21b28ec
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-02-03 16:15:51 -0800

    Update descope.ts

--- src/descope.ts
+++ src/descope.ts
@@ -2,7 +2,7 @@ import DescopeClient from '@descope/node-sdk';
 
 const DESCOPE_PROJECT_ID = process.env.DESCOPE_PROJECT_ID;
 const DESCOPE_MANAGEMENT_KEY = process.env.DESCOPE_MANAGEMENT_KEY;
-const DESCOPE_BASE_URL = process.env.DESCOPE_BASE_URL;
+const DESCOPE_BASE_URL = process.env.DESCOPE_BASE_URL || "https://api.descope.com";
 if (!DESCOPE_PROJECT_ID || !DESCOPE_MANAGEMENT_KEY) {
     throw new Error('DESCOPE_PROJECT_ID and DESCOPE_MANAGEMENT_KEY must be set');
 }
